aee8d72f8dac6fdb76f11bd29baf8045ecd2ce06
yurkaronin <yurkaronin@gmail.com>
2024-05-08 22:05:03 +0300

    +

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 console.log('Файл скриптов подключён!');
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM загружен');
+});
+
+
 // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
 // получить в консоли элемент, по которому кликнули
 document.addEventListener('click', e => console.log(e.target));
--- js/parts/form-validation.js
+++ js/parts/form-validation.js
@@ -1,44 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('input[type="tel"]').forEach(input => {
-    // Сохраняем оригинальное значение поля
-    let originalValue = input.value;
+  // Получаем все элементы input с типом "tel"
+  var phoneInputs = document.querySelectorAll('input[type="tel"]');
 
-    // Обработчик для форматирования номера
-    function formatPhoneNumber() {
-      let value = input.value.replace(/\D+/g, ''); // Удаляем все нецифровые символы
-      let formattedValue = '+7 ';
+  // Проходим по каждому из найденных элементов
+  phoneInputs.forEach(function (input) {
+    // Настройки маски
+    var maskOptions = {
+      mask: '+{7}(000)000-00-00'
+    };
 
-      if (value.length > 1) {
-        formattedValue += '(' + value.slice(1, 4);
-      }
-      if (value.length >= 4) {
-        formattedValue += ') ' + value.slice(4, 7);
-      }
-      if (value.length >= 7) {
-        formattedValue += '-' + value.slice(7, 9);
-      }
-      if (value.length >= 9) {
-        formattedValue += '-' + value.slice(9, 11);
-      }
-
-      input.value = formattedValue.slice(0, 18); // Ограничиваем длину форматированного номера
-    }
-
-    // Обработчик для редактирования номера
-    input.addEventListener('input', function (event) {
-      if (input.value.length < originalValue.length) {
-        // Если пользователь удаляет символы, возвращаем оригинальное значение
-        input.value = originalValue;
-      } else {
-        // Иначе форматируем введенное значение
-        formatPhoneNumber();
-      }
-    });
+    // Инициализация маски
+    var mask = IMask(input, maskOptions);
 
     // Обработчик для проверки длины номера при отправке формы
     input.closest('form').addEventListener('submit', function (event) {
-      let digitsCount = input.value.replace(/\D+/g, '').length;
+      let digitsCount = input.value.replace(/\D+/g, '').length; // Удаляем все нецифровые символы и считаем оставшиеся цифры
       if (digitsCount < 11) {
+        // Если количество цифр меньше 11, отменяем отправку формы и выводим сообщение
         event.preventDefault();
         alert('Введите корректный номер телефона. Минимум 11 цифр. Пример: +7 918 089 60 09');
       }
